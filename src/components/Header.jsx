f4c24ff689675e2ae2ce36b87dffd5fdefddfd52
TANNER MECK <steve.meck246@gmail.com>
2021-12-13 17:35:18 -0800

    header setup

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import './header.css'
+
+const Header = () => {
+    return (
+        <>
+            <div>
+                <img src='https://alchemy-guestbook.netlify.app/static/media/guestbook.b8329dbf.png' style={{height: 50 }}alt='book' />
+            </div>
+            <div>
+                <h3>Sign In Friend</h3>
+            </div>
+        </>
+    )
+}
+export default Header;
